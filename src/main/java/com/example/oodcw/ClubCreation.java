e3f3c60548e14b310e7bab883fc247860313c773
Sabeeha Mazahir <125579608+SabeehaMazahir@users.noreply.github.com>
2023-11-17 01:18:32 +0530

    Club Creation and Managing controllers

---
+++ src/main/java/com/example/oodcw/ClubCreation.java
@@ -0,0 +1,32 @@
+package com.example.oodcw;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+public class ClubCreation {
+
+        @FXML
+        private TextField clubCategory;
+
+        @FXML
+        private TextField clubMotto;
+
+        @FXML
+        private TextField clubName;
+
+        @FXML
+        private TextField nameOfClubAdvisor;
+
+        @FXML
+        private Button saveClub;
+
+        @FXML
+        void saveClubClick(ActionEvent event) {
+
+        }
+
+
+    }
+
+
---
+++ src/main/java/com/example/oodcw/ManageClubs.java
@@ -0,0 +1,29 @@
+package com.example.oodcw;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ManageClubs {
+
+    @FXML
+    private TextField clubName;
+
+    @FXML
+    private Button deleteClub;
+
+    @FXML
+    private Button editClub;
+
+    @FXML
+    void deleteClubClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editClubClick(ActionEvent event) {
+
+    }
+
+}
