f9f32e25dad3bc78d00cc31bd8ac75414f3cb769
nuharameez <127706172+nuharameez@users.noreply.github.com>
2023-11-20 21:30:06 +0530

    join club

--- src/main/java/com/example/oodcw/JoinClub.java
+++ src/main/java/com/example/oodcw/JoinClub.java
@@ -1,4 +1,8 @@
 package com.example.oodcw;
 
+
+//get clubs from database and populate to the choice box
+//when join button is clicked, the club needs to be saved with the students name and other details
+//when clear is clicked a new window needs to be opened.
 public class JoinClub {
 }
