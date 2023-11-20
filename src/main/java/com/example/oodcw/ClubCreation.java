c717abad48c3764923e5c747229bd94cd827f923
Sabeeha Mazahir <125579608+SabeehaMazahir@users.noreply.github.com>
2023-11-20 23:47:42 +0530

    Club Creation Controller

--- src/main/java/com/example/oodcw/ClubCreation.java
+++ src/main/java/com/example/oodcw/ClubCreation.java
@@ -1,9 +1,15 @@
 package com.example.oodcw;
 
+//import javafx.beans.property.StringPropertyBase;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+
 public class ClubCreation {
 
         @FXML
@@ -18,15 +24,21 @@ public class ClubCreation {
         @FXML
         private TextField nameOfClubAdvisor;
 
-        @FXML
-        private Button saveClub;
+
 
         @FXML
-        void saveClubClick(ActionEvent event) {
+        public void saveClubClick(ActionEvent event) throws Exception {
+                String Name = clubName.getText();
+                String Category = clubCategory.getText();
+                String Advisor = nameOfClubAdvisor.getText();
+                String Motto = clubMotto.getText();
+
+                // insert into the database
+                checkDetailsEvent(Name, Category, Advisor, Motto);
+        }
+
+        public void checkDetailsEvent(String Name, String Category, String Advisor, String Motto) {
 
         }
 
-
-    }
-
-
+}
