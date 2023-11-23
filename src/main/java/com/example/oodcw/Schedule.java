b5e9bca63064d7bc9b04711e1e5f96fd3de0ba8a
NabeelahF <nabfad.fm@gmail.com>
2023-11-24 00:32:32 +0530

    Nabeelah

--- src/main/java/com/example/oodcw/Schedule.java
+++ src/main/java/com/example/oodcw/Schedule.java
@@ -7,4 +7,33 @@ public abstract class Schedule {
     private Date date;
     private String venue;
 
+    public Schedule(String name, Date date, String venue) {
+        this.name = name;
+        this.date = date;
+        this.venue = venue;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getVenue() {
+        return venue;
+    }
+
+    public void setVenue(String venue) {
+        this.venue = venue;
+    }
 }
