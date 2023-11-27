a1d07756a0dede829acb154ae455abded27795f2
NabeelahF <nabfad.fm@gmail.com>
2023-11-27 12:13:41 +0530

    Nabeelah

--- src/main/java/com/example/oodcw/Activity.java
+++ src/main/java/com/example/oodcw/Activity.java
@@ -1,30 +1,75 @@
 package com.example.oodcw;
 
-import java.util.Date;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.time.LocalDate;
 
-public class Activity extends Schedule{
+public class Activity extends Schedule {
     private int maxParticipants;
     private String description;
 
-    public Activity(String name, Date date, String venue, int maxParticipants, String description) {
-        super(name, date, venue);
+    private String type;
+
+
+    public Activity(int ScheduleID, String name, LocalDate date, String venue, int maxParticipants, String description) {
+        super( ScheduleID,name, date, venue);
         this.maxParticipants = maxParticipants;
         this.description = description;
+        this.type= "Activity";
     }
 
+    @Override
+    public String getType() {
+        return type;
+    }
+
+
     public int getMaxParticipants() {
+
         return maxParticipants;
     }
 
     public void setMaxParticipants(int maxParticipants) {
+
         this.maxParticipants = maxParticipants;
     }
 
     public String getDescription() {
+
         return description;
     }
 
     public void setDescription(String description) {
+
         this.description = description;
+    }
+
+    public void ActivitysaveToDatabase() {
+        try (Connection connection = DatabaseController.getConnection()) {
+            // Insert into 'schedule' table
+            String scheduleQuery = "INSERT INTO schedule (ScheduleID, Name, Date, Venue, Type) VALUES (?, ?, ?, ?, ?)";
+            try (PreparedStatement scheduleStatement = connection.prepareStatement(scheduleQuery)) {
+                scheduleStatement.setInt(1, getScheduleID());
+                scheduleStatement.setString(2, getName());
+                scheduleStatement.setObject(3, getDate());
+                scheduleStatement.setString(4, getVenue());
+                scheduleStatement.setString(5, "Activity"); // Set type to 'Activity'
+
+                scheduleStatement.executeUpdate();
+            }
+
+            // Insert into 'activity' table
+            String activityQuery = "INSERT INTO activity (ScheduleID, MaxParticipants, Description) VALUES (?, ?, ?)";
+            try (PreparedStatement activityStatement = ((Connection) connection).prepareStatement(activityQuery)) {
+                activityStatement.setInt(1, getScheduleID());
+                activityStatement.setInt(2, getMaxParticipants());
+                activityStatement.setString(3, getDescription());
+
+                activityStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
