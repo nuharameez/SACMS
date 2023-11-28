050208b6b83633adb4239c45ed94508d3e94b6e6
NabeelahF <nabfad.fm@gmail.com>
2023-11-28 09:03:49 +0530

    Nabeelah

--- src/main/java/com/example/oodcw/DatabaseController.java
+++ src/main/java/com/example/oodcw/DatabaseController.java
@@ -10,25 +10,4 @@ public class DatabaseController {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
     }
-
-    public boolean IDExists(int ID) {
-        try (Connection connection = DatabaseController.getConnection()) {
-            String query = "SELECT COUNT(*) FROM schedule WHERE ScheduleID = ?";
-            try (PreparedStatement statement = connection.prepareStatement(query)) {
-                statement.setInt(1, ID);
-                try (ResultSet resultSet = statement.executeQuery()) {
-                    if (resultSet.next()) {
-                        int count = resultSet.getInt(1);
-                        return count > 0;
-                    }
-                } catch (SQLException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
 }
-
